1627ee17c430ceab290d363368b42c3bc34c76de
Brandon-Harris <brandonharris177@gmail.com>
2020-06-17 14:12:21 -0600

    single number done tests passing

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,25 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
-
-    pass
+    count = 1
+    newArray = []
+    while len(newArray) == 0:
+        # print(arr[0], arr[count])
+        if count >= len(arr):
+            newArray.append(arr[0])
+        elif arr[0] == arr[count]:
+            # print("fired", arr[0], arr[count])
+            arr.pop(count)
+            arr.pop(0)
+            # print(arr)
+            count = 1
+        else:
+            count = count+1
+ 
+    # print(newArray)
+    return newArray[0]
+        
+# single_number([1, 1, 2, 2, 3, 4, 4])
 
 
 if __name__ == '__main__':
