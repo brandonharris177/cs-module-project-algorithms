1c84db439c89ad591e3361a8946ebacbd1bc9b90
Brandon-Harris <brandonharris177@gmail.com>
2020-06-17 14:45:01 -0600

    moving zeros working and tests passing

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,8 +5,19 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    length = len(arr)
+    new_arr = [0] * length
+    count = 0
 
+    for value in arr:
+        if value != 0:
+            new_arr[count] = value
+            count = count +1
+
+    # print(new_arr)
+    return new_arr
+
+# moving_zeroes([0, 3, 1, 0, -2])
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
