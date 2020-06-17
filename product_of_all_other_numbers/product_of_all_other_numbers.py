0f044e11b1eb3d3c9ba1c9a6aa8e23c01202fbb0
Brandon-Harris <brandonharris177@gmail.com>
2020-06-17 15:14:44 -0600

    product of all other numbers tests passing

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -4,9 +4,20 @@ Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
+    new_list = []
+    total_product = 1
+    for x in arr: 
+        total_product = total_product * x  
 
-    pass
+    for value in arr:
+        new_list.append(int(total_product/value))
 
+    # print(new_list)
+    return new_list
+
+# product_of_all_other_numbers([1, 2, 3, 4, 5])
+
+# product_of_all_other_numbers([2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8])
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
