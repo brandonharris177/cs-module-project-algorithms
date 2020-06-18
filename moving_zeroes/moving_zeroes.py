318d9ca17ee8053b040b4418a884fe599746d6b0
Brandon-Harris <brandonharris177@gmail.com>
2020-06-18 13:33:10 -0600

    eating cookies, single number, and moving zeros all meeting specificaions and tests passing

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -26,9 +26,22 @@ Returns: a List of integers
 #     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
 
 def moving_zeroes(arr):
-    for i in range(len(arr)):
-        x = arr[i]
-        if x != 0:
-            arr = [x] + arr[:i] + arr[i+1]
+    start = 0
+    end = len(arr)-1
 
+    while start < end:
+        if arr[end] == 0:
+            end -=1
+        elif arr[start] != 0:
+            start +=1
+        elif arr[start] == 0 and arr[end] != 0:
+            arr[start], arr[end] = arr[end], arr[start]
+            end -=1
+            start +=1
     return arr
+
+if __name__ == '__main__':
+    # Use the main function here to test out your implementation
+    arr = [0, 3, 1, 0, -2]
+
+    print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
